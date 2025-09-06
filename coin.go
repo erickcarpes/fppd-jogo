7ffb5645a74070b572b92e78cd075e7d839b4403
Erick <erickcarpesmmc@gmail.com>
2025-09-06 20:21:15 -0300

    Refactor coin spawning logic and add portal management functionality

--- coin.go
+++ coin.go
@@ -3,7 +3,9 @@ package main
 import "time"
 import "math/rand"
 
-func coinSpawner(jogo *Jogo) {
+func coinManager(jogo *Jogo) {
+	var existingCoin bool = false
+	var posicaoMoedaX, posicaoMoedaY int
 
 	spawnTicker := time.NewTicker(10 * time.Second)
 	defer spawnTicker.Stop()
@@ -11,26 +13,59 @@ func coinSpawner(jogo *Jogo) {
 	for {
 		select {
 		case <-spawnTicker.C:
-			spawnCoin(jogo)
+			if existingCoin {
+				clearCoin(jogo, posicaoMoedaX, posicaoMoedaY)
+				existingCoin = false
+			}
+			spawned, x, y := spawnCoin(jogo)
+			if spawned {
+				existingCoin = true
+				posicaoMoedaX = x
+				posicaoMoedaY = y
+			}
 		case <-gameOverChannel:
 			return
 		}
 	}
 }
 
-func spawnCoin(jogo *Jogo) {
+func spawnCoin(jogo *Jogo) (bool, int, int) {
 
 	maxY := len(jogo.Mapa)
 	maxX := len(jogo.Mapa[0])
+	
+	// Tenta spawnar a moeda indefinidamente até achar uma posição válida
+	for {
+		// Gera uma posição aleatória para x e y
+		x := rand.Intn(maxX)
+		y := rand.Intn(maxY)
 
-	x := rand.Intn(maxX)
-	y := rand.Intn(maxY)
-
-	cmd := func(jogo *Jogo) {
+		// Verifica se a posição é válida (vazio e não tangível)
 		if !jogo.Mapa[y][x].tangivel && jogo.Mapa[y][x].simbolo != Moeda.simbolo {
-            jogo.Mapa[y][x] = Moeda
-        }
-	}
 
-	mapChannel <- cmd
+			// Escreve o comando atualizando o mapa
+			cmd := func(jogo *Jogo) {
+				jogo.Mapa[y][x] = Moeda
+			}
+			
+			// Envia o comando para o mapManager
+			mapChannel <- cmd
+			// Retorna a posição onde a moeda foi spawnada
+			return true, x, y
+		}
+	}
+}
+
+func clearCoin(jogo *Jogo, x int, y int) {
+
+	// Verifica se ainda há uma moeda na posição
+	if jogo.Mapa[y][x].simbolo == Moeda.simbolo {
+
+		// Escreve o comando atualizando o mapa
+		cmd := func(jogo *Jogo) {
+			jogo.Mapa[y][x] = Vazio
+		}
+		// Envia o comando para o mapManager
+		mapChannel <- cmd
+	}
 }
